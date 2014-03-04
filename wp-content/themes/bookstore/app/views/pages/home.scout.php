928f59b53144eeb22cd875e28fed930a05542aee
Julien Lambé <julien@jlambe.be>
2014-03-04 19:01:27 +0100

    Add popular books section.

--- wp-content/themes/bookstore/app/views/pages/home.scout.php
+++ wp-content/themes/bookstore/app/views/pages/home.scout.php
@@ -16,9 +16,53 @@
 		</div>
 	</div>
 	<!-- END BOOK PROMO -->
-
 	<!-- POPULAR -->
-	
+	<div id="popular-books" class="wrapper">
+		<div class="bks-title-box">
+			<h1>Popular</h1>
+			<a href="#" title="Books" class="bks-link">&gt; all books</a>
+		</div>
+		<div id="popular-container">
+			<ul class="books">
+				<li>
+					<div class="book">
+						<h3>Design is a job</h3>
+						<a href="#" class="book-featured-box">
+							<img src="<?php echo(themosisAssets()); ?>/images/book001.jpg" alt="Book featured image" width="266" height="146">
+						</a>
+						<p>Co-founder of Mule Design and raconteur Mike Monteiro wants to help you do your job better.</p>
+						<div class="button-box">
+							<a href="#" class="tiny-button">Buy</a>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="book">
+						<h3>The elements of content strategy</h3>
+						<a href="#" class="book-featured-box">
+							<img src="<?php echo(themosisAssets()); ?>/images/book002.jpg" alt="Book featured image" width="266" height="146">
+						</a>
+						<p>Co-founder of Mule Design and raconteur Mike Monteiro wants to help you do your job better.</p>
+						<div class="button-box">
+							<a href="#" class="tiny-button">Buy</a>
+						</div>
+					</div>
+				</li>
+				<li class="last">
+					<div class="book">
+						<h3>HTML5 for web designers</h3>
+						<a href="#" class="book-featured-box">
+							<img src="<?php echo(themosisAssets()); ?>/images/book003.jpg" alt="Book featured image" width="266" height="146">
+						</a>
+						<p>Co-founder of Mule Design and raconteur Mike Monteiro wants to help you do your job better.</p>
+						<div class="button-box">
+							<a href="#" class="tiny-button">Buy</a>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
 	<!-- END POPULAR -->
 
 @include('footer')
