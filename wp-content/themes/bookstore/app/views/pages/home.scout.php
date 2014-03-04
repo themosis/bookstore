fe182a336595b88b65a97f77fdc2e58833cf78a6
Julien Lambé <julien@jlambe.be>
2014-03-04 15:46:58 +0100

    Static implementation of the book promo.

--- wp-content/themes/bookstore/app/views/pages/home.scout.php
+++ wp-content/themes/bookstore/app/views/pages/home.scout.php
@@ -1,7 +1,24 @@
 @include('header')
 
 	<!-- BOOK PROMO -->
-	
+	<div id="bks-promo">
+		<div class="wrapper">
+			<div class="promo-wrapper">
+				<div class="promo-container">
+					<h1>Sass for web designers</h1>
+					<h5>By Dan Cederholm</h5>
+					<a href="#" class="big-button">Buy book</a>
+				</div>
+				<div class="promo-media">
+					<img src="<?php echo(themosisAssets()); ?>/images/book001.png" alt="Demo Book" width="399" height="435">
+				</div>
+			</div>
+		</div>
+	</div>
 	<!-- END BOOK PROMO -->
+
+	<!-- POPULAR -->
+	
+	<!-- END POPULAR -->
 
 @include('footer')
