caebc8e40712ae6f9552f1636893f9c96d246928
Julien Lambé <julien@jlambe.be>
2014-03-05 21:38:08 +0100

    Add about page datas WordPress admin.

---
+++ wp-content/plugins/themosis-datas/app/admin/about.php
@@ -0,0 +1,27 @@
+<?php
+
+/*-----------------------------------------------------------------------*/
+// ABOUT PAGE CUSTOM DATAS
+/*-----------------------------------------------------------------------*/
+$about = get_page_by_path('about');
+
+if (themosisIsPost($about->ID)) {
+
+	/*-----------------------------------------------------------------------*/
+	// TEAM METABOX
+	/*-----------------------------------------------------------------------*/
+	Metabox::make('Team', 'page')->set(array(
+
+		Field::infinite('collaborators', array(
+
+			Field::text('full-name', array('title' => 'Full name')),
+			Field::text('job'),
+			Field::media('pic', array('info' => 'Select <b>Team</b> size for the image.'))
+
+		), array('info' => 'Set your collaborator list.'))
+
+	));
+
+}
+
+?>
