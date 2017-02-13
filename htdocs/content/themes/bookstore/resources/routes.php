eaf690135b4026c20fec7d450efe9b8ae5dd91a9
Julien Lambé <julien@jlambe.be>
2017-02-13 09:16:49 +0100

    Complete books archive and single pages with Twig templates.

--- htdocs/content/themes/bookstore/resources/routes.php
+++ htdocs/content/themes/bookstore/resources/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Dev\Bookstore\Books\Models\Books;
+
 /*
  * Define your routes and which views to display
  * depending of the query.
@@ -17,11 +19,37 @@
  */
 Route::match(['get', 'post'], 'front', 'Pages@home');
 
-// The books page
-Route::get('postTypeArchive', array('bks-books', 'uses' => 'Books@archive'));
+/*
+ * Books archive page.
+ *
+ * @param \WP_Post $post Last WP_Post instance from archive loop.
+ * @param \WP_Query $query
+ */
+Route::match(['get', 'post'], 'postTypeArchive', function ($post, \WP_Query $query) {
+    return view('twig.books.archive', [
+        'books'	=> $query->get_posts()
+    ]);
+});
 
-// Single book page
-Route::get('singular', array('bks-books', 'uses' => 'Books@single'));
+/*
+ * Single book page.
+ * The "Books" model is auto instanciated and ready for use. Its dependency is only the WP_Query class which
+ * is also auto instanciated in order to run the model.
+ *
+ * @param Books $books The books model defined from the plugin.
+ * @param \WP_Post $post The book WP_Post instance.
+ * @param \WP_Query $query
+ */
+Route::get('singular', ['bks-books', function (Books $books, \WP_Post $post, \WP_Query $query) {
+    return view('twig.books.single', [
+        'book' => $post,
+        'books' => $books->find([
+            'posts_per_page'	=> 3,
+            'post__not_in'		=> [$post->ID],
+            'orderby'			=> 'rand'
+        ])->get()
+    ]);
+}]);
 
 // About page
 Route::get('page', array('about', 'uses' => 'Pages@about'));
