e21abca05eb7c2803152bd979a9a8c58a1aa67c1
Julien <julien@jlambe.be>
2017-04-30 17:40:14 +0200

    Refactor blog latest blade template.

--- htdocs/content/themes/bookstore/resources/views/blade/blog/latest.blade.php
+++ htdocs/content/themes/bookstore/resources/views/blade/blog/latest.blade.php
@@ -9,25 +9,31 @@
         @if(isset($latest_articles))
             <div class="bks-blog-latest">
                 <ul>
-                    <?php
-                        $i = 0;
+                    @php
                         $modulo = 2;
-                    ?>
-                    @query(array('post_type' => 'post', 'posts_per_page' => 2))
-                        <li <?php if(1 == $i % $modulo){ echo('class="last"'); } ?>>
-                            <article class="home-article">
-                                <h5>{{ get_post_time('j F Y', true, Loop::id()) }}</h5>
-                                <h2>{{ Loop::title() }}</h2>
-                                <p>{{ Loop::excerpt() }}</p>
+                    @endphp
+                    @foreach($latest_articles as $i => $post)
+                        @if(1 == $i % $modulo)
+                            <li class="last">
+                        @else
+                            <li>
+                        @endif
+                            <article class="blog-article">
+                                <h5>{{ get_post_time('j F Y', true, $post->ID) }}</h5>
+                                <h2>{{ $post->post_title }}</h2>
+                                @if(!empty($post->post_excerpt))
+                                    <p>{{ $post->post_excerpt }}</p>
+                                @else
+                                    <p>{!! wp_trim_words($post->post_content, 10) !!}</p>
+                                @endif
                                 <div class="link-box">
-                                    <a href="{{ Loop::link() }}" class="tiny-button yellow" title="Read more">Read more</a>
+                                    <a href="{{ get_permalink($post->ID)}}" class="tiny-button yellow" title="{{ __("Read more", THEME_TEXTDOMAIN) }}">{{ __("Read more", THEME_TEXTDOMAIN) }}</a>
                                 </div>
                             </article>
                         </li>
-                        <?php $i++; ?>
-                    @endquery
+                    @endforeach
                 </ul>
             </div>
-        @endif
+        @endif  
     </div>
 </div>
