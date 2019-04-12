9a53daba63a9c9c07ad34c02f9c02be152996206
Julien Lambé <julien@themosis.com>
2019-04-12 16:09:51 +0200

    Set default theme to bookstore.

--- config/wordpress.php
+++ config/wordpress.php
@@ -19,14 +19,14 @@
 | @link https://api.wordpress.org/secret-key/1.1/salt/
 |
 */
-define('AUTH_KEY', 'put your unique phrase here');
-define('SECURE_AUTH_KEY', 'put your unique phrase here');
-define('LOGGED_IN_KEY', 'put your unique phrase here');
-define('NONCE_KEY', 'put your unique phrase here');
-define('AUTH_SALT', 'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT', 'put your unique phrase here');
-define('NONCE_SALT', 'put your unique phrase here');
+define('AUTH_KEY',         'SxJ9%e7HcbM2,N*l1mekvMho%kf>-xFy!@|C@7]tu$6wJiCO^z+h#39fi~%^M8}P');
+define('SECURE_AUTH_KEY',  'i6l*b+{ghnz`g[0:+|6=r9|+N{aKA~-2HT5Q|t,A:3|/ia?#7:0bd[^=LQr3<l4.');
+define('LOGGED_IN_KEY',    '#0KgeP*%1FcyY7h]Ro<8Y>;`o:u0-^!XCK;ai,NGJx1eTf+?$R--)|!+0aLM.VmC');
+define('NONCE_KEY',        'OOQw(9ul^Gzx8:h2o5lNs.`.CaHKcf&e+|++O8YR;)t<HD(-c*^u6/9a4?Ls),7c');
+define('AUTH_SALT',        '&[ha|Ukq={9p8=GjIO=*i1ybURBx8UamK2Qmi6D_Zi.~d984FtOx)e5@)cU8Ui7G');
+define('SECURE_AUTH_SALT', '?ue1h7_ ,4_ t3p{7z4kyy*+: mN:?8&#@(p9)05)0]>;3v7*0Xgd/1*?||q%xC$');
+define('LOGGED_IN_SALT',   'T75z.}a^t,L~1aKc9K3&5MO/U{z?Fj?T4:n/)qL9Iv!eQ0{-E06aO4E+ _1~8nC0');
+define('NONCE_SALT',       'eh}L=bG^*d%S8nG1tApGR4f=}:_O@$DX9kD+7rPu=kb!vx?CFGj`5]shR@^td#-<');
 
 /*
 |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ define('DISALLOW_FILE_EDIT', true);
 | WordPress default theme
 |--------------------------------------------------------------------------
 */
-define('WP_DEFAULT_THEME', 'themosis');
+define('WP_DEFAULT_THEME', 'bookstore');
 
 /*
 |--------------------------------------------------------------------------
